82e31007f096678e89f3e0c19ce930262c0f2680
Habiballah Heazrehee <hahh20@student.bth.se>
2021-07-20 18:01:10 +0200

    Added and edited login page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
-import TextField from "./components/Forms/TextField";
-import Select from "./components/Forms/Select";
+import "./style.css";
+import Login from "./Pages/Login";
 
 function App() {
-  const [name, setName] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`submitting the: ${name}`);
-  };
   return (
     <>
-      <header>
-        <div className="container">
-          <div className="flex-item">
-            <nav>
-              <ul>
-                <li>Logo</li>
-                <li>
-                  <a href="https://google.com">Google</a>
-                </li>
-                <li>
-                  <a href="https://google.com">Google</a>
-                </li>
-                <li>
-                  <a href="https://google.com">Google</a>
-                </li>
-                <li>
-                  <a href="https://google.com">Google</a>
-                </li>
-                <li>
-                  <a href="https://google.com">Google</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className="flex-item">
-            <input type="text" placeholder="Search ..." />
-          </div>
-        </div>
-      </header>
-      <section>Section</section>
-      <footer>Footer</footer>
+      <Login />
     </>
   );
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Navigation from "../components/Navigation/Navigation";
-
-export const Login = (props) => {
-    return (
-      <>
-        <Navigation  />
-      </>
-    );
+import TextField from "../components/Forms/TextField";
+import Button from "../components/Button/Button";
+export default function Login(props) {
+  return (
+    <>
+      <div className="white-box">
+        <TextField
+          label="Usernmame:"
+          placeholder="myEmail@mydomain.com"
+          type="email"
+        />
+        <TextField label="Password:" type="password" />
+        <Button cssClass="btn btn-primary login-btn" text="Login" />
+        <p className="center">Forgot your password?</p>
+      </div>
+    </>
+  );
 }
